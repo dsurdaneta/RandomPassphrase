e6e9c76b7a4e8f02717207fe5015760113671806
dsurdaneta <dsurdaneta@users.noreply.github.com>
2025-11-14 17:31:37 +0100

    add logic to ensure at least 1 lowercase 1 uppercase and 1 digit

--- passphrase-generator.py
+++ passphrase-generator.py
@@ -1,7 +1,13 @@
 import string
 import random
 
-def passphrase_generator(size = 8, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+def passphrase_generator(size = 12, alphabet = string.ascii_letters + string.digits):
+    while True:
+        password = ''.join(random.choice(alphabet) for _ in range(size))
+        if (any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password)>= 3):
+            break
+    return password
 
 print(passphrase_generator())
