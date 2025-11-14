2a163fc55b77d6cc5589009f539315ce41a39cd8
dsurdaneta <dsurdaneta@users.noreply.github.com>
2025-11-14 17:18:32 +0100

    add method to generate a random string that can be used as a password

---
+++ passphrase-generator.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+def passphrase_generator(size = 8, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+print(passphrase_generator())
